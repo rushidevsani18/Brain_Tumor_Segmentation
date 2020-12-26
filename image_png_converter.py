562d7feedb45f40acae76342e03a137e9d3dc720
rushidevsani18 <73092903+rushidevsani18@users.noreply.github.com>
2020-12-26 19:13:01 +0530

    Update image_png_converter.py

--- image_png_converter.py
+++ image_png_converter.py
@@ -13,12 +13,6 @@ from os import makedirs
 from os.path import isdir
 from errno import EEXIST
 
-__author__ = "Cesare Catavitello"
-__license__ = "MIT"
-__version__ = "1.0.1"
-__maintainer__ = "Cesare Catavitello"
-__email__ = "cesarec88@gmail.com"
-__status__ = "Production"
 
 
 def mkdir_p(path):
